e7188bd1d8602b29779cae9e99bc3bdd94b80947
Jackson Phanvilay <jphanvilay.1@gmail.com>
2024-08-22 17:49:06 -0500

    added updated code for svg files and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ImageCreator
+
+An application to create a small svg(image) based off user input.
+
+## Links
+
+https://drive.google.com/file/d/1Cq3ooYPeA-YJONkz2FGWXOZNcIBRfB73/view
+
+https://github.com/JPhanvilay/ImageCreator
+
+Below is an image created through the application. Can also be found in the repository in the images folder.
+![svg image.](./Images/logo.svg)
--- index.js
+++ index.js
@@ -1,37 +1,76 @@
+const fs = require("fs");
 const inquirer = require("inquirer");
-const Shape = require("./lib/shape");
+const { Circle, Square, Triangle } = require("./lib/shape");
 
-inquirer.prompt([
-        {
-            type: "input",
-            name: "characters",
-            message: "What text would you like to enter? (3 Characters Maximum)",
-            validate: function(answer){
-                if(answer.length > 3){
-                    return false;
-                }
-                return true;
-            }
-        },
-        {
-            type: "input",
-            name: "textcolor",
-            message: "What color do you want to make the text? (Must be a valid color or a hexadecimal",
-        },
-        {
-            type: "list",
-            name: "shape",
-            message: "What shape should the logo be?",
-            choices: ["Triange", "Square", "Circle"]
-        },
-        {
-            type: "input",
-            name: "shapecolor",
-            message: "What color should the shape be?",
-        }
-    ]
-)
+// Function to generate the SVG logo
+function generateSVGLogo({ text, color, shape, textColor }) {
+ let shapeInstance;
 
-.then((answers) => {
-    console.log(answers);
-})
+ // Switch case to select the appropriate shape class based on user input
+ switch (shape) {
+  case "circle":
+   shapeInstance = new Circle(color, text, textColor);
+   break;
+  case "square":
+   shapeInstance = new Square(color, text, textColor);
+   break;
+  case "triangle":
+   shapeInstance = new Triangle(color, text, textColor);
+   break;
+  default:
+   console.error("Invalid shape selected.");
+   process.exit(1);
+ }
+
+ // Render the shape as SVG
+ return shapeInstance.render();
+}
+
+// Function to save the generated SVG to a file
+function saveSVG(svgContent) {
+ fs.writeFileSync("logo.svg", svgContent);
+ console.log(`Generated logo.svg`);
+}
+
+// Main function to handle user input and generate the logo
+async function main() {
+ // Prompt the user with questions using inquirer
+ const answers = await inquirer.prompt([
+  {
+   type: "input",
+   name: "text",
+   message: "Enter the text for the logo (max 3 characters):",
+   validate: function (input) {
+    if (input.length > 3) {
+     return "Text can be up to 3 characters.";
+    }
+    return true;
+   },
+  },
+  {
+   type: "input",
+   name: "color",
+   message: "Enter the color for the shape (e.g., #ff5733):",
+  },
+  {
+   type: "input",
+   name: "textColor",
+   message: "Enter the color for the text (e.g., #ffffff):",
+  },
+  {
+   type: "list",
+   name: "shape",
+   message: "Choose the shape for the logo:",
+   choices: ["circle", "square", "triangle"],
+  },
+ ]);
+
+ // Generate the SVG logo based on user input
+ const svgContent = generateSVGLogo(answers);
+
+ // Save the SVG to a file
+ saveSVG(svgContent);
+}
+
+// Call the main function to start the process
+main();
